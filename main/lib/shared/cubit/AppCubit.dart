99f063054ba385da421dc4d6963130207f6fe92d
salah137 <boksaloh@gmail.com>
2021-07-06 12:20:01 +0100

    INIT

--- main/lib/shared/cubit/AppCubit.dart
+++ main/lib/shared/cubit/AppCubit.dart
@@ -47,7 +47,7 @@ class AppCubit extends Cubit<AppState> {
             "CREATE TABLE Elevelangs (id INTEGER PRIMARY KEY, name TEXT, category INTEGER, english INTEGER, french INTEGER)");
         // payments
         db.execute(
-            "CREATE TABLE payment (id INTEGER PRIMARY KEY, eleveName TEXT, matiere TEXT, payedlastmonth INTEGER, nonPayedMonths INTEGER)");
+            "CREATE TABLE payment (id INTEGER PRIMARY KEY, eleveName TEXT, matiere TEXT,hepaythisMonth INTEGER, payedlastmonth INTEGER, nonPayedMonths INTEGER)");
       },
       onOpen: (
         db,
@@ -100,18 +100,18 @@ class AppCubit extends Cubit<AppState> {
         print("data primaire is inserted");
         if (math)
           await txn.rawInsert(
-            "INSERT INTO payment (eleveName,matiere,payedlastmonth,nonPayedMonths) VALUES(?,?,?,?)",
-            [name, "math", 1, 0],
+            "INSERT INTO payment (eleveName,matiere,hepaythisMonth,payedlastmonth,nonPayedMonths) VALUES(?,?,?,?)",
+            [name, "math", 0, 1, 0],
           );
         if (french)
           await txn.rawInsert(
-            "INSERT INTO payment (eleveName,matiere,payedlastmonth,nonPayedMonths) VALUES(?,?,?,?)",
-            [name, "francer", 1, 0],
+            "INSERT INTO payment (eleveName,matiere,hepaythisMonth,payedlastmonth,nonPayedMonths) VALUES(?,?,?,?)",
+            [name, "francer", 0, 1, 0],
           );
         if (arabic)
           await txn.rawInsert(
-            "INSERT INTO payment (eleveName,matiere,payedlastmonth,nonPayedMonths) VALUES(?,?,?,?)",
-            [name, "arabe", 1, 0],
+            "INSERT INTO payment (eleveName,matiere,hepaythisMonth,payedlastmonth,nonPayedMonths) VALUES(?,?,?,?)",
+            [name, "arabe", 1, 0, 0],
           );
       },
     );
@@ -119,7 +119,8 @@ class AppCubit extends Cubit<AppState> {
     getdata(database!);
   }
 
-  void addCollegeStudent(String name, bool french, bool math, bool pc, bool svt) async {
+  void addCollegeStudent(
+      String name, bool french, bool math, bool pc, bool svt) async {
     int mathisTrue = math ? 1 : 0;
     int frenchisTrue = french ? 1 : 0;
     int pcisTrue = pc ? 1 : 0;
@@ -133,32 +134,32 @@ class AppCubit extends Cubit<AppState> {
         );
         if (math)
           await txn.rawInsert(
-            "INSERT INTO payment (eleveName,matiere,payedlastmonth,nonPayedMonths) VALUES(?,?,?,?)",
-            [name, "math", 1, 0],
+            "INSERT INTO payment (eleveName,matiere,hepaythisMonth,payedlastmonth,nonPayedMonths) VALUES(?,?,?,?)",
+            [name, "math", 0, 1, 0],
           );
         if (french)
           await txn.rawInsert(
-            "INSERT INTO payment (eleveName,matiere,payedlastmonth,nonPayedMonths) VALUES(?,?,?,?)",
-            [name, "francer", 1, 0],
+            "INSERT INTO payment (eleveName,matiere,hepaythisMonth,payedlastmonth,nonPayedMonths) VALUES(?,?,?,?)",
+            [name, "francer", 0, 1, 0],
           );
         if (pc)
           await txn.rawInsert(
-            "INSERT INTO payment (eleveName,matiere,payedlastmonth,nonPayedMonths) VALUES(?,?,?,?)",
-            [name, "pysics", 1, 0],
+            "INSERT INTO payment (eleveName,matiere,hepaythisMonth,payedlastmonth,nonPayedMonths) VALUES(?,?,?,?)",
+            [name, "pysics", 0, 1, 0],
           );
         if (svt)
           await txn.rawInsert(
-            "INSERT INTO payment (eleveName,matiere,payedlastmonth,nonPayedMonths) VALUES(?,?,?,?)",
-            [name, "svt", 1, 0],
+            "INSERT INTO payment (eleveName,matiere,hepaythisMonth,payedlastmonth,nonPayedMonths) VALUES(?,?,?,?)",
+            [name, "svt", 0, 1, 0],
           );
       },
     );
     emit(InsertDataCollegeState());
     getdata(database!);
-
   }
 
-  void addLyceeStudent(String name, bool french, bool math, bool pc, bool svt) async {
+  void addLyceeStudent(
+      String name, bool french, bool math, bool pc, bool svt) async {
     int mathisTrue = math ? 1 : 0;
     int frenchisTrue = french ? 1 : 0;
     int pcisTrue = pc ? 1 : 0;
@@ -172,29 +173,28 @@ class AppCubit extends Cubit<AppState> {
         );
         if (math)
           await txn.rawInsert(
-            "INSERT INTO payment (eleveName,matiere,payedlastmonth,nonPayedMonths) VALUES(?,?,?,?)",
-            [name, "math", 1, 0],
+            "INSERT INTO payment (eleveName,matiere,hepaythisMonth,payedlastmonth,nonPayedMonths) VALUES(?,?,?,?)",
+            [name, "math", 0, 1, 0],
           );
         if (french)
           await txn.rawInsert(
-            "INSERT INTO payment (eleveName,matiere,payedlastmonth,nonPayedMonths) VALUES(?,?,?,?)",
-            [name, "francer", 1, 0],
+            "INSERT INTO payment (eleveName,matiere,hepaythisMonth,payedlastmonth,nonPayedMonths) VALUES(?,?,?,?)",
+            [name, "francer", 0, 1, 0],
           );
         if (pc)
           await txn.rawInsert(
-            "INSERT INTO payment (eleveName,matiere,payedlastmonth,nonPayedMonths) VALUES(?,?,?,?)",
-            [name, "pysics", 1, 0],
+            "INSERT INTO payment (eleveName,matiere,hepaythisMonth,payedlastmonth,nonPayedMonths) VALUES(?,?,?,?)",
+            [name, "pysics", 0, 1, 0],
           );
         if (svt)
           await txn.rawInsert(
-            "INSERT INTO payment (eleveName,matiere,payedlastmonth,nonPayedMonths) VALUES(?,?,?,?)",
-            [name, "svt", 1, 0],
+            "INSERT INTO payment (eleveName,matiere,hepaythisMonth,payedlastmonth,nonPayedMonths) VALUES(?,?,?,?)",
+            [name, "svt", 0, 1, 0],
           );
       },
     );
     emit(InsertDataLangsState());
     getdata(database!);
-
   }
 
   void addLangsState(String name, bool isBig, bool english, bool french) async {
@@ -205,21 +205,23 @@ class AppCubit extends Cubit<AppState> {
     database!.transaction(
       (txn) async {
         txn.rawInsert(
-            "INSERT INTO Elevelangs(name,category,english,french) VALUES(?,?,?,?)",
-            [name,category,isEnglish,isFrench],
+          "INSERT INTO Elevelangs(name,category,english,french) VALUES(?,?,?,?)",
+          [name, category, isEnglish, isFrench],
         );
+
       },
     );
-    emit(InsertDataLangsState(),);
+    emit(
+      InsertDataLangsState(),
+    );
     getdata(database!);
-
   }
 
   void ChekPayments() async {
-    if(DateTime.now().day == 1){
-      for(int i = 0; i < payement.length; i++) {
-        if(payement[i]["payedlastmonth"] == 0){
-          payement[i]["nonPayedMonths"]+= payement[i]["nonPayedMonths"];
+    if (DateTime.now().day == 1) {
+      for (int i = 0; i < payement.length; i++) {
+        if (payement[i]["payedlastmonth"] == 0) {
+          payement[i]["nonPayedMonths"] += 1;
         }
       }
     }
