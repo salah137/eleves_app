f9d34afd10e4390c9309ffaf9b8b0e7429513635
salah137 <boksaloh@gmail.com>
2021-07-07 15:25:22 +0100

    Initial commit

--- main/lib/screens/EleveScreen.dart
+++ main/lib/screens/EleveScreen.dart
@@ -86,7 +86,58 @@ class _ElevesScreenState extends State<ElevesScreen> {
     return BlocConsumer<AppCubit, AppState>(
         builder: (ctx, state) {
           AppCubit cubit = BlocProvider.of(ctx);
+  bool listEnglisBigs = false;
+  bool listEnglisKids = false;
+  bool listFrenchBigs = false;
+  bool listFrenchKids = false;
+  // primaire settings
 
+  bool listPrimaireArabic1 = false;
+  bool showPrimaireArabic2 = false;
+  bool showPrimaireArabic3 = false;
+  bool showPrimaireArabic4 = false;
+  bool showPrimaireArabic5 = false;
+  bool showPrimaireArabic6 = false;
+  bool showPrimaireFrench1 = false;
+  bool showPrimaireFrench2 = false;
+  bool showPrimaireFrench3 = false;
+  bool showPrimaireFrench4 = false;
+  bool showPrimaireFrench5 = false;
+  bool showPrimaireFrench6 = false;
+  bool showPrimaireMath1 = false;
+  bool showPrimaireMath2 = false;
+  bool showPrimaireMath3 = false;
+  bool showPrimaireMath4 = false;
+  bool showPrimaireMath5 = false;
+  bool showPrimaireMath6 = false;
+  // College settings
+
+  bool showCollegeFrench1 = false;
+  bool showCollegeFrench2 = false;
+  bool showCollegeFrench3 = false;
+  bool showCollegeMath1 = false;
+  bool showCollegeMath2 = false;
+  bool showCollegeMath3 = false;
+  bool showCollegePc1 = false;
+  bool showCollegePc2 = false;
+  bool showCollegePc3 = false;
+  bool showCollegeSvt1 = false;
+  bool showCollegeSvt2 = false;
+  bool showCollegeSvt3 = false;
+  // LyccE
+  bool showLyccesFrench1 = false;
+  bool showLyccesFrench2 = false;
+  bool showLyccesFrench3 = false;
+  bool showLyccesMath1 = false;
+  bool showLyccesMath2 = false;
+  bool showLyccesMath3 = false;
+  bool showLyccesPc1 = false;
+  bool showLyccesPc2 = false;
+  bool showLyccesPc3 = false;
+  bool showLyccesSvt1 = false;
+  bool showLyccesSvt2 = false;
+  bool showLyccesSvt3 = false;
+  
           return Scaffold(
               body: SafeArea(
                 child: Padding(
