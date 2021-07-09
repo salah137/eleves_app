4fece3bbdcf21723cbe42d3fb86828bbec5d2248
salah137 <boksaloh@gmail.com>
2021-07-09 17:02:16 +0100

    Initial commit

--- main/lib/screens/ELeveInfoScreen.dart
+++ main/lib/screens/ELeveInfoScreen.dart
@@ -17,71 +17,67 @@ class EleveInfo extends StatelessWidget {
           bool showPrimair = AppCubit.elevePrimaire.contains(element);
 
           return Scaffold(
-              body: SafeArea(
-                  child: SingleChildScrollView(
-                    child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-                     Container(
-                        decoration: BoxDecoration(
-                            color: Colors.pink,
-
-                            borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(15),
-                            bottomRight: Radius.circular(15)
-                          )
-                        ),
-                        width: double.infinity,
-                        height: 400,
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            CircleAvatar(
-                              radius: 80,
-                              child: Text('${element["name"][0]}' , style: TextStyle(
-                                fontSize: 50
-                              ),),
-                            ),
-                            SizedBox(
-                              height: 20,
-                            ),
-                            Text(
-                              element["name"],
+            body: SafeArea(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.pink,
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(15),
+                              bottomRight: Radius.circular(15))),
+                      width: double.infinity,
+                      height: 400,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          CircleAvatar(
+                            radius: 80,
+                            child: Text(
+                              '${element["name"][0]}',
                               style: TextStyle(fontSize: 50),
-                            )
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            element["name"],
+                            style: TextStyle(fontSize: 50),
+                          )
+                        ],
+                      ),
+                    ),
+                    if (AppCubit.elevePrimaire.contains(element))
+                      SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            if (element["math"] == 1)
+                              buildMatiereItem(text: "Math", ontap: () {}),
+                            if (element["french"] == 1)
+                              buildMatiereItem(text: "francais", ontap: () {}),
+                            if (element["arabic"] == 1)
+                              buildMatiereItem(text: "Arabic", ontap: () {})
                           ],
                         ),
                       ),
-                    if (AppCubit.elevePrimaire.contains(element))
-                      SingleChildScrollView(
-                        child:  Column(
-                            children: [
-                              if (element["math"] == 1)
-                                buildMatiereItem(text: "Math", ontap: () {}),
-                              if (element["french"] == 1)
-                                buildMatiereItem(text: "francais", ontap: () {}),
-                              if (element["arabic"] == 1)
-                                buildMatiereItem(text: "Arabic", ontap: () {})
-                            ],
-                          ),
-                        ),
-
                     if (AppCubit.elevecollegeNames.contains(element["name"]))
-                Column(
-                            children: [
-                              if (element["math"] == 1)
-                                buildMatiereItem(text: "Math", ontap: () {}),
-                              if (element["french"] == 1)
-                                buildMatiereItem(text: "francais", ontap: () {}),
-                              if (element['physic'] == 1)
-                                buildMatiereItem(text: "physic", ontap: () {}),
-                              if (element["svt"] == 1)
-                                buildMatiereItem(text: 'SVT', ontap: () {})
-                            ],
-                          ),
-
+                      Column(
+                        children: [
+                          if (element["math"] == 1)
+                            buildMatiereItem(text: "Math", ontap: () {}),
+                          if (element["french"] == 1)
+                            buildMatiereItem(text: "francais", ontap: () {}),
+                          if (element['physic'] == 1)
+                            buildMatiereItem(text: "physic", ontap: () {}),
+                          if (element["svt"] == 1)
+                            buildMatiereItem(text: 'SVT', ontap: () {})
+                        ],
+                      ),
                     if (AppCubit.elevelyceesNames.contains(element["name"]))
                       Column(
                         children: [
@@ -95,43 +91,38 @@ class EleveInfo extends StatelessWidget {
                             buildMatiereItem(text: 'SVT', ontap: () {})
                         ],
                       ),
-                    if (
-                        AppCubit.elevelangsNames.contains(element["name"]))
-                     Column(
-                          children: [
-                            if (element["french"] == 1 ||
-                                AppCubit.elevelangs[AppCubit.elevelangsNames
-                                        .indexOf(element["name"])]["french"] ==
-                                    1)
-                              buildMatiereItem(text: "french", ontap: () {}),
-                            if (element["english"] == 1 ||
-                                AppCubit.elevelangs[AppCubit.elevelangsNames
-                                        .indexOf(element["name"])]["english"] ==
-                                    1)
-                              buildMatiereItem(text: "english", ontap: () {}),
-                          ],
-                        ),
-
+                    if (AppCubit.elevelangsNames.contains(element["name"]))
+                      Column(
+                        children: [
+                          if (element["french"] == 1 ||
+                              AppCubit.elevelangs[AppCubit.elevelangsNames
+                                      .indexOf(element["name"])]["french"] ==
+                                  1)
+                            buildMatiereItem(text: "french", ontap: () {}),
+                          if (element["english"] == 1 ||
+                              AppCubit.elevelangs[AppCubit.elevelangsNames
+                                      .indexOf(element["name"])]["english"] ==
+                                  1)
+                            buildMatiereItem(text: "english", ontap: () {}),
+                        ],
+                      ),
                     if (AppCubit.elevesCalculNames.contains(element["name"]))
-
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                buildMatiereItem(text: "Calcule Mentale", ontap: () {}),
-                              ],
-                            ),
-
-            ],
-          ),
-                  ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          buildMatiereItem(
+                              text: "Calcule Mentale", ontap: () {}),
+                        ],
+                      ),
+                  ],
                 ),
+              ),
+            ),
             floatingActionButton: FloatingActionButton.extended(
               icon: Icon(Icons.add),
               label: Text('Ajouter Une Matiere'),
-              onPressed: (){
-                
-              },
+              onPressed: () {},
             ),
           );
         },
